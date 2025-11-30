aca7d2bdb19162a55ab04fd4503178ae3e4bc524
ghambrecht <gabriel.hambrecht@gmail.com>
2025-11-30 21:25:03 +0100

    started with ex02

---
+++ ex02/Account.cpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/30 20:34:59 by ghambrec          #+#    #+#             */
+/*   Updated: 2025/11/30 21:24:47 by ghambrec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account(void)
+{
+
+}
+
+Account::~Account(void)
+{
+    
+}
+
+Account::Account(int initial_deposit)
+{
+    
+}
+
+void	Account::_displayTimestamp(void)
+{
+    // hier weiter
+}
+
+int	Account::getNbAccounts(void)
+{
+    return (_nbAccounts);
+}
+
+int Account::getTotalAmount(void)
+{
+    return (_totalAmount);
+}
+
+int Account::getNbDeposits(void)
+{
+    return (_totalAmount);
+}
+
+int Account::getNbWithdrawals(void)
+{
+    return (_totalNbWithdrawals);
+}
+
+void Account::displayAccountsInfos(void)
+{
+    
+}
+
+void Account::makeDeposit( int deposit )
+{
+
+}
+
+bool Account::makeWithdrawal( int withdrawal )
+{
+
+}
+
+int Account::checkAmount( void ) const
+{
+
+}
+
+void Account::displayStatus( void ) const
+{
+    
+}
